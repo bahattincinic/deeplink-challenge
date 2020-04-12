fe403496bcd9aa62f3504bf4a874eaecfe06e76f
Bahattin Cinic <bahattin@adphorus.com>
2020-04-12 12:32:01 +0300

    Added some docs

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 const env = process.env.NODE_ENV || 'development';
-const src = env === 'production' ? './build/app' : './src/app';
 
 require('dotenv').config();
 
@@ -11,6 +10,6 @@ if (env === 'development') {
 }
 
 const port = require('./src/config').appPort;
+const app = require('./src/app').default;
 
-const app = require(src).default;
 app.listen(port, () => console.log(`API Server listening on http://localhost:${port}`));
--- src/config.js
+++ src/config.js
@@ -5,17 +5,32 @@ http://www.rabbitmq.com/tutorials/tutorial-one-javascript.html
 */
 export const queueConf = {
   brokerUrl: process.env.BROKER_URL || 'amqp://172.18.0.1',
-  durable: true,
+  durable: true, // the queue will survive broker restarts
   deliveryMode: 2, // 1 (non-persistent) 2 (persistent)
-  prefetchCount: 1,
+  prefetchCount: 1, // how many items we fetch at the same time.
   queueName: 'deeplink',
 };
 
+/*
+Default API version.
+*/
 export const apiVersion = 'v1';
+
+/*
+Database connection string.
+https://sequelize.org/master/manual/getting-started.html
+* */
 export const databaseUrl = (
   process.env.DATABASE_URL || 'postgres://postgres:postgres@172.18.0.1:5432/challenge'
 );
 
+/*
+Redis connection string.
+https://github.com/NodeRedis/node-redis#options-object-properties
+ */
 export const cacheUrl = process.env.CACHE_URL || 'redis://172.18.0.1:6379';
 
+/*
+Api Port.
+ */
 export const appPort = process.env.PORT || 4000;
