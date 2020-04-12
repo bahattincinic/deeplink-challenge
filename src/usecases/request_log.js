54e82b1b3fe22a5280c1dcbb571f61953ea15bff
Bahattin Cinic <bahattin@adphorus.com>
2020-04-12 17:36:54 +0300

    Added doc

--- src/usecases/request_log.js
+++ src/usecases/request_log.js
@@ -1,6 +1,9 @@
 import Queue from '../queue/amqp';
 
 export default async (url, status, request, response) => {
+  // it can be added to KoaJS middleware to log every request. i have only 4 endpoints.
+  // that's why I didn't do this.
+
   const client = await Queue.init();
 
   const data = {
